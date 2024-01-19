b4ccccbc832d564ca401f6cf441eda86690fb5d9
watanabe ryunosuke <46173232+berry8848@users.noreply.github.com>
2024-01-19 13:32:28 +0900

    Update CheckDistance.py

--- modules/CheckDistance.py
+++ modules/CheckDistance.py
@@ -1,26 +1,19 @@
 import math
-import sys
 
 class CheckDistance:
     '''応力値から点間距離を求め，点間距離内に他の点が含まれるか否かを判断する'''
-    def __init__(self, ALLOWABLE_STRESS):
+    def __init__(self, ALLOWABLE_STRESS, RADIUS):
         self.ALLOWABLE_STRESS = ALLOWABLE_STRESS
-        self.max_density = -999999
-        self.min_density = 999999
+        self.r = RADIUS
 
     def check_distance(self, fixed_points, candidate_point, stress):
         density = stress_to_density(stress, self.ALLOWABLE_STRESS) # 応力から密度の変換
-        long = density_to_long(density) # 密度から点間距離の変換
-
-        if density > self.max_density:
-            self.max_density = density
-        if density < self.min_density:
-            self.min_density = density
+        long = density_to_long(density, self.r) # 密度から点間距離の変換
         # 点間距離内に他の点が含まれているか否かを判定．
         # 点間距離内に他の点が含まれていたらFalseを返す
-        b_x = candidate_point.x 
-        b_y = candidate_point.y
-        b_z = candidate_point.z
+        b_x = candidate_point[0]
+        b_y = candidate_point[1]
+        b_z = candidate_point[2]
         check = True
         for a in fixed_points:
             a_x = a[0]
@@ -31,17 +24,13 @@ class CheckDistance:
                 check = False
                 break
         return check
-    
-    def print_max_min_density(self):
-        print('max_density : ', self.max_density)
-        print('min_density : ', self.min_density)
-    
+        
 # 応力と密度の関係式．※関係式が微妙なため，臨時で別の関数
 def stress_to_density(stress, ALLOWABLE_STRESS):
     density = abs(stress) / ALLOWABLE_STRESS
     return density
 
 # 密度と点間距離の関係式
-def density_to_long(density):
-    long = 6 * 0.1 * math.sqrt(math.sqrt(2)*math.pi/ density)
+def density_to_long(density, r):
+    long = 6 * r * math.sqrt(math.sqrt(2)*math.pi/ density)
     return long
